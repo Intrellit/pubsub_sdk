de697b4770821785c50a6ce2af3542bccc92436a
Marek Michalik <marekmichalik43@gmail.com>
2024-03-21 12:07:10 +0100

    Send event

--- src/pubsub_sdk/event.py
+++ src/pubsub_sdk/event.py
@@ -1,6 +1,19 @@
 import os
 
+import requests
+
 
 def read_event(envelope):
     if envelope.get("secret") == os.getenv("PUBSUB_SECRET"):
         return envelope.get("type"), envelope.get("body")
+
+
+def send_event(type, body):
+    requests.post(
+        "https://pubsub.jpnt.tech/ingress",
+        json={
+            "secret": os.getenv("PUBSUB_SECRET"),
+            "type": type,
+            "body": body,
+        },
+    )

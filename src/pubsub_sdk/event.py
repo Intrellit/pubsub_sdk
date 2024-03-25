436750f568aae59929063f6bc684282690ef4e25
Marek Michalik <marekmichalik43@gmail.com>
2024-03-25 11:35:36 +0100

    Return empty when auth fails

--- src/pubsub_sdk/event.py
+++ src/pubsub_sdk/event.py
@@ -6,6 +6,7 @@ import requests
 def read_event(envelope):
     if envelope.get("secret") == os.getenv("PUBSUB_SECRET"):
         return envelope.get("type"), envelope.get("body")
+    return None, None
 
 
 def send_event(type, body, url="https://pubsub.jpnt.tech/ingress"):

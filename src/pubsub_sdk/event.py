b9740105c9805e499a56f89ca0186e53331f2532
Marek Michalik <marekmichalik43@gmail.com>
2024-03-27 10:10:11 +0100

    Allow for custom ingress URL

--- src/pubsub_sdk/event.py
+++ src/pubsub_sdk/event.py
@@ -10,11 +10,11 @@ def read_event(envelope):
     return None, None
 
 
-def send_event(type, body, id=None):
+def send_event(type, body, id=None, url="https://pubsub.jpnt.tech/ingress"):
     if not id:
         id = round(time.time() * 1000)
     requests.post(
-        "https://pubsub.jpnt.tech/ingress",
+        url,
         json={
             "secret": os.getenv("PUBSUB_SECRET"),
             "type": type,
